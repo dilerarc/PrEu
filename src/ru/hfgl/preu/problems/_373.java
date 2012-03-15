56df9b937f9cad8f92a4f13de45cd282ffc0dde5
Valeriy Puryshev <dilerarc@gmail.com>
2012-03-15 21:35:36 +0400

    _373 added, not solved

---
+++ src/ru/hfgl/preu/problems/_373.java
@@ -0,0 +1,43 @@
+package ru.hfgl.preu.problems;
+
+import ru.hfgl.preu.utils.Testable;
+
+public class _373 implements Testable {
+
+    @Override
+    public String getResult() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void doOperation() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void setup() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
+
+class Triangle {
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    private int a, b, c;
+
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
